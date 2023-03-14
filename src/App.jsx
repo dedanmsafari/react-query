183c9fefef164985a31d6d002735d129ac4f2a03
dedanmsafari <dedan.msafari@gmail.com>
2023-03-14 04:50:15 +0300

    app file changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Navigation from "./components/Navigation.component";
 import HomePage from "./components/Home.page";
 import RQSuperHeroesPage from "./components/RQSuperHeroes.page";
 import SuperHeroesPage from "./components/SuperHeroes.page";
+import RQIndividualHeroPage from "./components/RQIndividualHero.page";
+import ParallelQueriesPage from "./components/ParallelQueriesPage";
+import DynamicParallelQueriesPage from "./components/DynamicParallelQueriesPage";
+import DependentQueriesPage from "./components/DependentQueriesPage";
 import ErrorPage from "./error-page";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -17,7 +21,20 @@ function App() {
         <Route path="/" element={<Navigation />}>
           <Route index element={<HomePage />} />
           <Route path="reactquery" element={<RQSuperHeroesPage />} />
+          <Route
+            path="/reactquery/:heroid"
+            element={<RQIndividualHeroPage />}
+          />
           <Route path="normalhooks" element={<SuperHeroesPage />} />
+          <Route path="parallelqueries" element={<ParallelQueriesPage />} />
+          <Route
+            path="dynamicparallelqueries"
+            element={<DynamicParallelQueriesPage heroids={[1, 2]} />}
+          />
+          <Route
+            path="dependentqueries"
+            element={<DependentQueriesPage email="dedan@gmail.com" />}
+          />
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
