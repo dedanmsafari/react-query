1b2e9c645aa80293576e9b243dcc9f389ed0fd51
dedanmsafari <dedan.msafari@gmail.com>
2023-03-14 16:36:35 +0300

    useSuperheroes page

--- src/hooks/useSuperHeros.js
+++ src/hooks/useSuperHeros.js
@@ -1,20 +1,30 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
 const fetchData = () => {
   return axios.get(" http://localhost:4000/superheroes");
 };
 
+const addSuperHero = (hero) => {
+  return axios.post(" http://localhost:4000/superheroes", hero);
+};
+
 const UseSuperHeros = ({ onSuccess, onError }) => {
   return useQuery({
     queryKey: ["getHeroes"],
-    queryFn: () => fetchData(),
+    queryFn: fetchData,
     onSuccess,
     onError,
     // enabled: false,
     // select: (data) => {
     //   return data.data.map((h) => h.name);
     // },
+  });
+};
+
+export const useAddSuperHeroData = () => {
+  return useMutation({
+    mutationFn: addSuperHero,
   });
 };
 
