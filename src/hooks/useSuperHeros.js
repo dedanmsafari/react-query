c867212f1877fe34881cf941be8fcfdca0acc410
dedanmsafari <dedan.msafari@gmail.com>
2023-03-15 13:23:19 +0300

    minor change to use superheroes

--- src/hooks/useSuperHeros.js
+++ src/hooks/useSuperHeros.js
@@ -1,12 +1,14 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { request } from "../utils/axios.utils";
 import axios from "axios";
-
 const fetchData = () => {
-  return axios.get(" http://localhost:4000/superheroes");
+  // return axios.get("http://localhost:4000/superheroes");
+  return request({ url: "/superheroes" });
 };
 
 const addSuperHero = (hero) => {
-  return axios.post(" http://localhost:4000/superheroes", hero);
+  // return axios.post(" http://localhost:4000/superheroes", hero);
+  return request({ url: "/superheroes", method: "post", data: hero });
 };
 
 const UseSuperHeros = ({ onSuccess, onError }) => {
@@ -22,9 +24,63 @@ const UseSuperHeros = ({ onSuccess, onError }) => {
   });
 };
 
+const onSuccess = (queryClient, data) => {
+  // return queryClient.invalidateQueries("getHeroes");
+  queryClient.setQueryData(["getHeroes"], (oldQueryData) => {
+    return {
+      ...oldQueryData,
+      data: [...oldQueryData.data, data.data],
+    };
+  });
+};
+
 export const useAddSuperHeroData = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: addSuperHero,
+
+    //optimistic update
+
+    onMutate: async (heroData) => {
+      //cancel previous request
+      await queryClient.cancelQueries({
+        queryKey: ["getHeroes"],
+      });
+      //save previous data
+      const previousData = queryClient.getQueryData(["getHeroes"]);
+      //do the update before
+      queryClient.setQueryData(["getHeroes"], (oldQueryData) => {
+        return {
+          ...oldQueryData,
+          data: [
+            ...oldQueryData.data,
+            { id: oldQueryData?.data?.length + 1, ...heroData },
+          ],
+        };
+      });
+
+      return {
+        previousData,
+      };
+    },
+    onError: (_error, _hero, context) => {
+      queryClient.setQueryData(["getHeroes"], context.previousData);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["getHeroes"],
+      });
+    },
+    // onSuccess: () => onSuccess(queryClient),
+    //onSuccess: () => onSuccess(queryClient),
+    // onSuccess: (data) => {
+    //   queryClient.setQueryData(["getHeroes"], (oldQueryData) => {
+    //     return {
+    //       ...oldQueryData,
+    //       data: [...oldQueryData.data, data.data],
+    //     };
+    //   });
+    // },
   });
 };
 
